6d5baa509a6853e70cdf09912f48c279db20cfa6
Adam Johnson <me@adamj.eu>
2018-02-24 20:08:28 -0500

    Add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
     packages=find_packages(exclude=['tests', 'tests.*']),
     zip_safe=False,
     install_requires=[],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     license='Apache License 2.0',
     include_package_data=True,
     classifiers=[
